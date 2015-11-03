aa31e661678ed7e5eeb538fc3c16e4d2cdd2425d
Václav Klusák <keo@keo.cz>
2015-11-03 16:17:03 +0100

    Use multipart upload to S3 for large files.

--- s3.py
+++ s3.py
@@ -1,5 +1,7 @@
 """Amazon S3."""
 
+import os
+
 from boto.s3 import connect_to_region
 
 
@@ -17,12 +19,29 @@ class S3Connection(object):
 
 class Bucket(object):
 
+    PART_LIMIT = (4 << 30)  # 4 GB
+
     def __init__(self, handle):
         self.handle = handle
 
     def put(self, source, target):
         key = self.handle.new_key(target)
-        key.set_contents_from_filename(source)
+        source_size = os.stat(source).st_size
+        if source_size <= self.PART_LIMIT:
+            key.set_contents_from_filename(source)
+            return
+        multipart = self.handle.initiate_multipart_upload(target)
+        try:
+            with open(source, 'rb') as fp:
+                offsets = xrange(0, source_size, self.PART_LIMIT)
+                for part, offset in enumerate(offsets, start=1):
+                    size = min(source_size, offset + self.PART_LIMIT) - offset
+                    fp.seek(offset)
+                    multipart.upload_part_from_file(fp, part, size=size)
+            multipart.complete_upload()
+        except:
+            multipart.cancel_upload()
+            raise
 
     def get(self, source, target):
         key = self.handle.get_key(source, validate=False)

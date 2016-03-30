236233013e85b1b93e049c4c94f99e41b7ff9eb4
MartinMikita <martin.mikita@klokantech.com>
2016-03-30 18:45:41 +0200

    Rewritten GCS wrapper - using Google discovery build API

--- gcs.py
+++ gcs.py
@@ -2,54 +2,95 @@
 
 import os
 import errno
+import httplib2
 
 from time import sleep
-from gcloud import storage
-from gcloud.exceptions import GCloudError
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+from apiclient.http import MediaIoBaseDownload
 
 class GcsConnection(object):
 
     def __init__(self):
-        self.connection = storage.Client()
+        self.credentials = GoogleCredentials.get_application_default()
 
     def bucket(self, name):
         for _ in range(6):
             try:
-                return Bucket(self.connection.get_bucket(name))
+                return Bucket(discovery.build('storage', 'v1', credentials=self.credentials), name)
             except IOError as e:
                 if e.errno == errno.EPIPE:
-                    self.connection = storage.Client()
+                    self.credentials = GoogleCredentials.get_application_default()
                 sleep(10)
-            except GCloudError:
-                sleep(30)
 
 
 
 class Bucket(object):
 
-    def __init__(self, handle):
+
+    CHUNK_SIZE = (500 << 20)  # 500 MB
+
+    def __init__(self, handle, name):
         self.handle = handle
+        self.name = name
 
     def put(self, source, target):
-        key = self.handle.blob(target)
-        key.upload_from_filename(source)
-        # multipart = self.handle.initiate_multipart_upload(target)
-        # try:
-        #     with open(source, 'rb') as fp:
-        #         offsets = xrange(0, source_size, self.PART_LIMIT)
-        #         for part, offset in enumerate(offsets, start=1):
-        #             size = min(source_size, offset + self.PART_LIMIT) - offset
-        #             fp.seek(offset)
-        #             multipart.upload_part_from_file(fp, part, size=size)
-        #     multipart.complete_upload()
-        # except:
-        #     multipart.cancel_upload()
-        #     raise
+        # Resumable has to be True, otherwise it is not working correctly
+        media = MediaFileUpload(source, chunksize=self.CHUNK_SIZE, resumable=True)
+        request = self.handle.objects().insert(
+            bucket=self.name,
+            name=target,
+            media_body=media)
+
+        response = None
+        num_retries = 5
+        while response is None:
+            error = None
+            try:
+                progress, response = request.next_chunk()
+            except HttpError, err:
+                if err.resp.status < 500:
+                    raise err
+                error = err
+            except (httplib2.HttpLib2Error, IOError), err:
+                error = err
+            if error:
+                if num_retries > 0:
+                    num_retries -= 1
+                    sleep(10)
+                else:
+                    raise error
+
 
     def get(self, source, target):
-        key = self.handle.get_blob(source)
-        key.download_to_filename(target)
+        # Use get_media instead of get to get the actual contents of the object.
+        # http://g.co/dev/resources/api-libraries/documentation/storage/v1/python/latest/storage_v1.objects.html#get_media
+        request = self.handle.objects().get_media(bucket=self.name, object=source)
+        with open(target, 'wb') as out_file:
+            downloader = MediaIoBaseDownload(out_file, request, chunksize=self.CHUNK_SIZE)
+            done = False
+            num_retries = 5
+            while not done:
+                error = None
+                try:
+                    status, done = downloader.next_chunk()
+                except HttpError, err:
+                    if err.resp.status < 500:
+                        raise err
+                    error = err
+                except (httplib2.HttpLib2Error, IOError), err:
+                    error = err
+                if error:
+                    if num_retries > 0:
+                        num_retries -= 1
+                        sleep(10)
+                    else:
+                        raise error
+
 
     def has(self, source):
-        key = self.handle.blob(source)
-        return key.exists()
+        request = self.handle.objects().get(bucket=self.name, object=source)
+        resp, _ = request.http.request(request.uri, 'GET')
+        return resp.status == 200

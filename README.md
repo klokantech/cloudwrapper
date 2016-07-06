eeca435a93d30862f8433d7303355d312299b973
MartinMikita <martin.mikita@klokantech.com>
2016-07-06 15:34:56 +0200

    Created python module structure. Added README, setup.py (#1)

---
+++ README.md
@@ -0,0 +1,34 @@
+# cloudwrapper
+
+Wrappers around cloud services for Amazon, Google and private cloud.
+
+Copyright (C) 2016 Klokan Technologies GmbH (http://www.klokantech.com/)
+Maintainer: Martin Mikita, martin.mikita@klokantech.com
+
+Modules:
+
+ - Amazon cloud services:
+   - *s3*: Amazon S3 storage.
+   - *sqs*: Amazon SQS queues.
+   - *cwl*: Amazon CloudWatch logs.
+
+ - Google cloud services:
+   - *gce*: Google Compute Engine instance metadata.
+   - *gcl*: Google Cloud Logging.
+   - *gcm*: Google Custom Metric (v2).
+   - *gcm3*: Google Custom Metric (v3).
+   - *gcs*: Google Cloud Storage.
+   - *gdm*: Google Deployment Manager using API v2.
+   - *gps*: Google PubSub using GCE Authentication.
+   - *gtq*: Google Task Pull Queues.
+
+ - Other cloud services:
+   - *btq*: BeansTalkd Queues.
+   - *idm*: Influx DB Metric.
+
+
+## Install
+
+```bash
+pip install https://github.com/klokantech/cloudwrapper/archive/master.zip
+```
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import sys
+
+import cloudwrapper
+
+with open('cloudwrapper/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+with open('README.md', **open_kwds) as f:
+    readme = f.read()
+
+setup(
+    name='cloudwrapper',
+    version=cloudwrapper.__version__,
+    description="Wrappers around cloud services for Amazon, Google and private cloud",
+    long_description=readme,
+    classifiers=[],
+    keywords='',
+    author='Klokan Technologies GmbH',
+    author_email='info@klokantech.com',
+    maintainer='Martin Mikita',
+    maintainer_email='martin.mikita@klokantech.com',
+    url='https://github.com/klokantech/cloudwrapper',
+    license='Copyright 2016 Klokan Technologies GmbH',
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    install_requires=[
+        'boto==2.39.0',
+        'requests==2.9.1',
+        'gcloud==0.13.0',
+        'beanstalkc==0.4.0',
+        'oauth2client==2.0.2',
+        'google-api-python-client==1.5.0',
+        'gcloud_taskqueue==0.1.2',
+    ],
+)
